51ae4fb6c90e7878be0ec4c0c123d58750e20c66
Wee Chuan <mr.ongwc@gmail.com>
2024-09-09 17:22:16 +0800

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+/* Importing react-router */
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+// Importing the components to the sda
+import Root from "./routes/root";
+import ErrorPage from './error-page';
+import Contact from "./routes/contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+  
+]);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
